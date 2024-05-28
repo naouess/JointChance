b0394ae8a80e3ce573f6d388208115945fbb6d27
Naouess <n.ouanes@posteo.de>
2024-05-28 12:22:58 +0100

    rename package

--- src/JointChance.jl
+++ src/JointChance.jl
@@ -1,4 +1,4 @@
-module JCC
+module JointChance
     using JuMP, MvNormalCDF, Random, Distributions, SpecialFunctions, LinearAlgebra
 
     export add_constraints_SRD, add_constraints_Genz, compute_with_SRD, compute_with_genz
