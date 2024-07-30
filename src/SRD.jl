254f14736432e11535c1ced8afb4f60821ec2a3d
Naouess <n.ouanes@posteo.de>
2024-07-31 01:41:51 +0200

    add documentation

--- src/SRD.jl
+++ src/SRD.jl
@@ -87,29 +87,29 @@ function ProbFunction(x, SampleOnSphere, mu)
 end
 
 """	
-	```julia
-	compute_with_SRD(j, κ, Σ, μ, size, rng)
-	```
+```julia
+compute_with_SRD(j, κ, Σ, μ, size, rng)
+```
 
-	This function computes a separable joint probability function and its gradient using 
-    Spherical Radial Decomposition. The function is defined as
-	
-	```math
-	f(x) = h(x(t) ≥ ξ(t) ∀t = j, ..., j+κ).
-	```
+This function computes a separable joint probability function and its gradient using 
+Spherical Radial Decomposition. The function is defined as
 
-	The function takes 6 arguments:
-	* `w` - index in Σ and μ from which the joint probability applies.
-	* `κ` - duration of an outage
-	* `Σ` - covariance matrix
-	* `μ` - mean vector
-	* `size` - sample
-	* `rng` - random number generator
+```math
+f(x) = h(x(t) ≥ ξ(t) ∀t = j, ..., j+κ).
+```
 
-	If `size` and `rng` are not specified, they take the default values `size = 5000` and `rng = MersenneTwister(1234)`.
-	With the latter, we fix the random number generator `rng` to guarantee the stability of the gradients in the solver iterations.
-	This tempers with the precision of the solution found, yet it is necessary for solver convergence.
-	# TODO formulate better here
+The function takes 6 arguments:
+* `w` - index in Σ and μ from which the joint probability applies.
+* `κ` - duration of an outage
+* `Σ` - covariance matrix
+* `μ` - mean vector
+* `size` - sample
+* `rng` - random number generator
+
+If `size` and `rng` are not specified, they take the default values `size = 5000` and `rng = MersenneTwister(1234)`.
+With the latter, we fix the random number generator `rng` to guarantee the stability of the gradients in the solver iterations.
+This tempers with the precision of the solution found, yet it is necessary for solver convergence.
+# TODO formulate better here
 
 """
 function compute_with_SRD(w, κ, Σ, μ, size, rng)
