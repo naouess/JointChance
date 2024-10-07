4112a0989d9868307a963932bbba365902c82321
Naouess <n.ouanes@posteo.de>
2024-10-07 16:49:35 +0200

    add test for default argument values & test for multiple JCC

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,17 +15,32 @@ p = 0.9
 
 x = [8; 6; 7; 5;]
 
-prob  = compute_with_Genz(1, κ, Σ, μ, 10000, MersenneTwister(1234))[1](x...)
-prob1 = compute_with_SRD(1, κ, Σ, μ, 10000, MersenneTwister(1234))[1](x...)
+prob_qmc  = compute_with_QMC(1, κ, Σ, μ, 10000, MersenneTwister(1234))[1](x...)
+prob_srd = compute_with_SRD(1, κ, Σ, μ, 10000, MersenneTwister(1234))[1](x...)
 
-v = round.(prob, digits=6)
-v1 = round.(prob1, digits=6)
+v1 = round.(prob_qmc, digits=6)
+v2 = round.(prob_srd, digits=6)
 
-@testset "JointChance.jl" begin
-    @test v ≈ 0.905384
-    @test v1 ≈ 0.903887
+prob_qmc1 = compute_with_QMC(1, κ, Σ, μ)[1](x...)
+prob_srd1 = compute_with_SRD(1, κ, Σ, μ)[1](x...)
+
+v3 = round.(prob_qmc1, digits=6)
+v4 = round.(prob_srd1, digits=6)
+
+prob_qmc_default = zeros()
+prob_srd_default = zeros()
+for j in [1, 2]
+    prob_qmc_default = [prob_qmc_default[j] compute_with_QMC(j, κ-1, Σ, μ)[1](x...)]
+    prob_srd_default = [prob_srd_default[j] compute_with_SRD(j, κ-1, Σ, μ)[1](x...)]
 end
 
-# TODO add tests for gradients
-# TODO add tests with more than one JCC
-# TODO add test with JuMP
+@testset "JointChance.jl" begin
+    @test v1 == 0.905384
+    @test v2 == 0.903887
+    @test v3 == 0.905395
+    @test v4 == 0.903956
+    @test round.(prob_qmc_default, digits=6) == [0.971658  0.920864]
+    @test round.(prob_srd_default, digits=6) == [0.971249  0.922053]
+end
+
+# TODO add test for the functions adding constraints to a JuMP model
